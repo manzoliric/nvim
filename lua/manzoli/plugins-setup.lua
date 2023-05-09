b38d3f73bbb19d138d84492cfeea603c50a60697
Richard Manzoli <manzolidev@gmail.com>
2023-05-09 13:36:52 -0300

    lint

--- lua/manzoli/plugins-setup.lua
+++ lua/manzoli/plugins-setup.lua
@@ -1,17 +1,17 @@
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
-  
+
   -- Plugins
   use 'nvim-tree/nvim-tree.lua'
-	use 'nvim-lua/plenary.nvim'
+  use 'nvim-lua/plenary.nvim'
   use 'nvim-tree/nvim-web-devicons'
   use {
     'goolord/alpha-nvim',
     requires = { 'nvim-tree/nvim-web-devicons' },
-    config = function ()
-			require'alpha'.setup(require'alpha.themes.startify'.config)
-		end
+    config = function()
+      require 'alpha'.setup(require 'alpha.themes.startify'.config)
+    end
   }
 
   -- apparence
